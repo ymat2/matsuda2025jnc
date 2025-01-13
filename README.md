5f6a954940c01477af0a9ee8d1508547becd712d
ymat2 <ymat2@users.noreply.github.com>
2025-01-13 14:18:05 +0900

    :fire: Remove unused code

--- README.md
+++ README.md
@@ -14,14 +14,14 @@ In this repository you find codes used in Matsuda *et al.* (2025) Population gen
 ### Bioinformatics tools
 
 - SRA-Toolkit
-- fastp (>=0.23.4)
-- samtools (>=1.18)
-- bwa (>=0.7.17)
-- bcftools (>=1.18)
-- plink (>=1.90b4)
-- admixture (>=1.3.0)
-- rapidnj (>=2.3.2)
-- treemix (>=1.13)
+- Fastp (>=0.23.4)
+- SAMtools (>=1.18)
+- BWA (>=0.7.17)
+- BCFtools (>=1.18)
+- PLINK (>=1.90b4)
+- Admixture (>=1.3.0)
+- RapidNJ (>=2.3.2)
+- Treemix (>=1.13)
 - [poppy](https://github.com/ymat2/poppy)
 
 ### R packages
--- src/mt8_phylo.sh
+++ src/mt8_phylo.sh
@@ -9,12 +9,10 @@
 
 shopt -s expand_aliases
 alias rapidnj="apptainer exec /usr/local/biotools/r/rapidnj:2.3.2--h4ac6f70_4 rapidnj"
-alias iqtree2="apptainer exec /usr/local/biotools/i/iqtree:2.2.6--h21ec9f0_0 iqtree2"
 
 workdir=~/popstr/phylo/mito
 vcf=~/popstr/vcf/popstr.mt.vcf.gz
 prefix=popstr.mt
-model=MFP
 
 [ ! -e ${workdir} ] && mkdir -p ${workdir}
 cd ${workdir}
@@ -24,7 +22,6 @@ cd ${workdir}
 rm ${prefix}.min4.fasta ${prefix}.min4.phy
 
 rapidnj ${prefix}.min4.sort.fasta -i fa -o t -b 100 --evolution-model kim -x ${prefix}.nj.tree
-iqtree2 -s ${prefix}.min4.sort.fasta -m ${model} -B 1000 -nt AUTO --prefix ${prefix} -redo
 
 /usr/bin/python3 ~/bin/MitoToolPy/mitotoolpy-seq.py -s chicken -r whole -i ${prefix}.min4.sort.fasta -o stdout > ${prefix}.mitotool
 /usr/bin/python3 ~/bin/MitoToolPy/mitotoolpy-seq.py -s chicken -r dloop -i ${prefix}.min4.sort.fasta -o stdout > ${prefix}.D.mitotool
